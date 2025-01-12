e97f36c2c7e9e7e7e2d04778e9e3217815beadb8
Jose Herminio Collas <goatstone@gmail.com>
2025-01-12 19:37:39 +0000

    Update deploy script to lock for one day

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -3,11 +3,12 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
+const oneDaySeconds = 24 * 60 * 60; // one day in seconds
+const unlockTimeEpoch = Math.floor(Date.now() / 1000) + oneDaySeconds;
 const ONE_GWEI: bigint = 1_000_000_000n;
 
 const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  const unlockTime = m.getParameter("unlockTime", unlockTimeEpoch);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
   const lock = m.contract("Lock", [unlockTime], {
