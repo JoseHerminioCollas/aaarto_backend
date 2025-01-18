b20122535f6c0cc748b531a5c886642e2349c7ec
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-01-18 13:13:22 -0500

    Add contract from openzeppelin wizard, add access control

---
+++ contracts/AaartoNFT.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/// @custom:security-contact aaarto@goatstone.com
+contract Aaarto is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 private _nextTokenId;
+    bool private mintEnabled;
+    
+    constructor(address defaultAdmin, address minter) ERC721("Aaarto", "AAARTO") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+        mintEnabled = true;
+    }
+
+    function preSafeMint(address to, string memory uri) public {
+        if(!mintEnabled) {
+            require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        }
+        _grantRole(MINTER_ROLE, msg.sender); 
+        safeMint(to, uri);
+    }
+
+    function safeMint(address to, string memory uri) private onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function setMintEnabled(bool enabled) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        mintEnabled = enabled;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
