de3d63e3c614979eb379417e6e40d4ae312f25fa
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-01-23 09:58:36 -0500

    Refactor, use .ts, update with import

--- scripts/interactNFT.ts
+++ scripts/interactNFT.ts
@@ -3,16 +3,18 @@ run with node interact.js
 SAMPLE CODE
  */
 
-const { ethers } = require("ethers");
-const AaartoNFTAbi = require("./path/to/your/AaartoNFTAbi.json");
+import { ethers } from "ethers" ;
+import AaartoNFTAbi from "../artifacts/contracts/AaartoNFT.sol/AaartoNFT.json" ;
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
 
 const provider = new ethers.providers.JsonRpcProvider("YOUR_SEPOLIA_RPC_URL");
-const signer = new ethers.Wallet("YOUR_PRIVATE_KEY", provider);
+const signer = new ethers.Wallet(SEPOLIA_PRIVATE_KEY, provider);
 
 const contractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
 const AaartoNFTContract = new ethers.Contract(contractAddress, AaartoNFTAbi, signer);
 
-async function mintNFT() {
+const mintNFT = async () => {
   const tokenURI = "ipfs://your-token-uri";
   const tx = await AaartoNFTContract.mint(signer.address, tokenURI);
   console.log("Minting transaction:", tx.hash);
