2b70637ab2b7a0f3d58457116e39c847e77bb9f3
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-06 10:37:37 -0500

    WIP

--- ignition/modules/AaartoNFTModule.ts
+++ ignition/modules/AaartoNFTModule.ts
@@ -1,13 +1,42 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { ethers } from "ethers";
 
-const AaartoNFTModule = buildModule("AaartoNFTModule", (m) => {
+const AaartoNFTModuleV2 = buildModule("AaartoNFTModuleV2", (m) => {
   const platformFee = "0.001"; // Platform fee in wei
 
-  const nft = m.contract("AaartoNFT",
-    [ethers.parseEther(platformFee)]
-  );
+  const nft = m.contract("AaartoNFT", {
+    args: [ethers.parseEther(platformFee)],
+    options: { gasLimit: 3000000 } // Adjust gas limit
+  });
+
   return { nft };
 });
 
-export default AaartoNFTModule;
+export default AaartoNFTModuleV2;
+
+// import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+// import { ethers } from "ethers";
+
+// const AaartoNFTModuleV2 = buildModule("AaartoNFTModuleV2", (m) => {
+//   const platformFee = "0.001"; // Platform fee in wei
+
+//   const nft = m.contract("AaartoNFT",
+//     [ethers.parseEther(platformFee)]
+//   );
+//   return { nft };
+// });
+
+// export default AaartoNFTModuleV2;
+// import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+// import { ethers } from "ethers";
+
+// const AaartoNFTModule = buildModule("AaartoNFTModule", (m) => {
+//   const platformFee = "0.001"; // Platform fee in wei
+
+//   const nft = m.contract("AaartoNFT",
+//     [ethers.parseEther(platformFee)]
+//   );
+//   return { nft };
+// });
+
+// export default AaartoNFTModule;
