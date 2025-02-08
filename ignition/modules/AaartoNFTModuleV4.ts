f68b1156ffc3bbe733ef9a25c7451875bbc851e2
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-08 05:42:51 -0500

    Update deploy scripts

--- ignition/modules/AaartoNFTModuleV4.ts
+++ ignition/modules/AaartoNFTModuleV4.ts
@@ -1,13 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { ethers } from "ethers";
 
-const AaartoNFTModuleV2 = buildModule("AaartoNFTModuleV2", (m) => {
+const AaartoNFTModuleV4 = buildModule("AaartoNFTModuleV3", (m) => {
   const platformFee = "0.001"; // Platform fee in wei
 
-  const nft = m.contract("AaartoNFTV2",
+  const nft = m.contract("AaartoNFTV4",
     [ethers.parseEther(platformFee)]
   );
   return { nft };
 });
 
-export default AaartoNFTModuleV2;
+export default AaartoNFTModuleV4;
