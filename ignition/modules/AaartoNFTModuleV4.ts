99f2846a24f169406cbe5989db7f6f4942064323
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-08 06:59:08 -0500

    Correct deploy script for Aaarto contract V4

--- ignition/modules/AaartoNFTModuleV4.ts
+++ ignition/modules/AaartoNFTModuleV4.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { ethers } from "ethers";
 
-const AaartoNFTModuleV4 = buildModule("AaartoNFTModuleV3", (m) => {
+const AaartoNFTModuleV4 = buildModule("AaartoNFTModuleV4", (m) => {
   const platformFee = "0.001"; // Platform fee in wei
 
   const nft = m.contract("AaartoNFTV4",
