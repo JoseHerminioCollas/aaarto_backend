545451040bb7ad26b156760e9beafddc1eb3913f
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-08 17:59:44 -0500

    update hardhat config with localhost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,9 @@ const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
